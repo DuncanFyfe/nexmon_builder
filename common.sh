9dea77fe5ee13af976b70d75850f8b217d194b83
Duncan Fyfe <duncanfyfe@domenlas.com>
2022-03-14 23:04:31 +0000

    Ansible playbook to prepare for building nexmon and nexmon_csi and initial bash scripts for doing the build.w

---
+++ common.sh
@@ -0,0 +1,107 @@
+_default_project_root=$(dirname $SCRIPT_DIR)
+export PROJECT_ROOT=${PROJECT_ROOT:-${_default_project_root}}
+
+ERR_UNKNOWN=99
+
+function debug {
+        # Arguments: debug message strings
+        # If LOG_LEVEL contains the substring "DEBUG" _AND_
+        # (DEBUG==ALL _OR_ DEBUG contains the scriptname as a substring)
+        # then echo the debug message.
+        if [ "X${LOG_LEVEL#*DEBUG}" != "X$LOG_LEVEL" ]; then
+                if [ "X$DEBUG" = "XALL" -o "X${DEBUG#*$SCRIPT_NAME}" != "X$DEBUG" ]; then
+                        local _tag=$SCRIPT_NAME
+                        echo "#DEBUG[$_tag] $@" 1>&2
+                fi
+        fi
+}
+
+function debugenv {
+        # Arguments: variable_name(s)
+        # If debugging is active echo the variable names and their
+        # environment variable values.
+        if [ "X$DEBUG" = "XALL" -o "X${DEBUG#*$SCRIPT_NAME}" != "X$DEBUG" ]; then
+                for _e in $@; do
+                        eval _v=\$${_e}
+                        echo "#DEBUG[$SCRIPT_NAME] ${_e}=${_v}" 1>&2
+                done
+        fi
+}
+
+function error {
+        # Arguments: status error [zero or more message strings]
+        # Echo any given given error message and exit with the given status.
+        # A default error status and message are used where either is not given.
+        local _status=$1
+        _status=${_status:-$ERR_UNKNOWN}
+        local _errmsg="${@:2}"
+        _errmsg=${_errmsg:="Exist Status $_status"}
+        echo "#ERROR[$SCRIPT_NAME] $_errmsg" 1>&2
+        exit $_status
+}
+
+function info() {          
+	echo "#INFO $@"
+}   
+
+function assert_var {
+        # Arguments: one environment variable name
+        # Test if the given environment variable is defined and of non-zero length
+        # Exit with status $ERR_ASSERT_VAR if it is not.
+        debug $FUNCNAME "$*"
+        local _var="$1"
+        if [ "X${_var}" == "X" ]; then
+                error $ERR_ASSERT_VAR "Failed assert var: ${_var}"
+        fi
+        eval _val=\$${_var}
+        if [ "X${_val}" == "X" ]; then
+                error $ERR_ASSERT_VAR "Failed assert var: ${_var}"
+        fi
+}
+
+function assert_vars {
+        # Arguments: one or more environment variable names.
+        # Test all named environment variable is defined and of non-zero length.
+        # Exit with status $ERR_ASSERT_VAR if not.
+        debug $FUNCNAME "$*"
+        for _v in $@; do
+                assert_var $_v
+        done
+}
+
+function assert_file {
+        # Arguments: one filename with path relative to $CWD.
+        # Test if the given path is a filename and exists
+        # Exit with status $ERR_ASSERT_FILE if it is not.
+        debug $FUNCNAME "$*"
+        local _f="$1"
+        assert_var _f
+        if [ "X${_f}" = "X" -o ! -f "${_f}" ]; then
+                error $ERR_ASSERT_FILE "Failed assert file: ${_f}"
+        fi
+}
+
+function assert_directory {
+        # Arguments: one directory with path relative to $CWD.
+        # Test if the given path is a directory and exists
+        # Exit with status $ERR_ASSERT_DIRECTORY if it is not.
+        debug $FUNCNAME "$*"
+        local _f="$1"
+        assert_var _d
+        if [ "X${_f}" != "X" -a ! -d "${_f}" ]; then
+                error $ERR_ASSERT_DIRECTORY "Failed assert directory: ${_f}"
+        fi
+}
+
+function assert_exists {
+        # Arguments: one filesystem path relative to $CWD.
+        # Test if the given path exists (but we don't care what it is)
+        # Exit with status $ERR_ASSERT_VAR if it is not.
+        debug $FUNCNAME "$*"
+        local _f="$1"
+        assert_var _f
+        if [ "X${_f}" != "X" -a ! -e "${_f}" ]; then
+                error $ERR_ASSERT_DIRECTORY "Failed assert exists: ${_f}"
+        fi
+}
+
---
+++ install.sh
@@ -0,0 +1,88 @@
+#!/bin/sh
+export SCRIPT=$(readlink -f "$0")
+export SCRIPT_DIR=$(dirname $SCRIPT)
+export SCRIPT_NAME=$(basename $SCRIPT)
+[ "X$DEBUG" = "XALL" -o "X${DEBUG#*$SCRIPT_NAME}" != "X$DEBUG" ] && echo "SCRIPT BEGIN $SCRIPT_NAME ${@:1}"
+. $SCRIPT_DIR/common.sh
+
+
+_pwd=$PWD
+# Some boxen have multiple wireless interfaces.
+# Removing wpasupplicate will just cause problems.
+# Tell it to ignore the first (wlan0) interface instead.
+# apt remove wpasupplicant -y
+echo "denyinterfaces wlan0" >> /etc/dhcpcd.conf
+info "Downloading Nexmon"
+git clone git@github.com:DuncanFyfe/nexmon.git
+git checkout python2
+cd nexmon
+NEXDIR=$(pwd)
+info "Done."
+
+info "Build libISL"
+assert_directory $NEXDIR/buildtools/isl-0.10
+cd $NEXDIR/buildtools/isl-0.10
+autoreconf -f -i
+./configure
+make
+make install
+assert_file /usr/lib/arm-linux-gnueabihf/libisl.so.10
+ln -s /usr/local/lib/libisl.so /usr/lib/arm-linux-gnueabihf/libisl.so.10
+info "Done."
+
+info "Building libMPFR"
+assert_directory $NEXDIR/buildtools/mpfr-3.1.4
+cd $NEXDIR/buildtools/mpfr-3.1.4
+autoreconf -f -i
+./configure
+make
+make install
+assert_file /usr/lib/arm-linux-gnueabihf/libmpfr.so.4
+ln -s /usr/local/lib/libmpfr.so /usr/lib/arm-linux-gnueabihf/libmpfr.so.4
+info "Done."
+
+info "Setting up Build Environment"
+cd $NEXDIR
+source setup_env.sh
+make
+info "Done."
+
+info "Downloading Nexmon_CSI"
+cd $NEXDIR/patches/bcm43455c0/7_45_189/
+git clone https://github.com/zeroby0/nexmon_csi.git
+assert_directory nexmon_csi
+info "Done."
+
+info "Building and installing Nexmon_CSI"
+cd nexmon_csi
+git checkout pi-5.4.51-plus
+make install-firmware
+info "Done."
+
+info "Installing makecsiparams"
+cd utils/makecsiparams
+make
+assert_file $PWD/makecsiparams
+ln -s $PWD/makecsiparams /usr/local/bin/mcp
+info "Done."
+
+info "Installing nexutil"
+cd $NEXDIR/utilities/nexutil
+make
+make install
+assert_file /usr/bin/nexutil
+info "Done."
+
+info "Setting up Persistance"
+cd $NEXDIR/patches/bcm43455c0/7_45_189/nexmon_csi/
+cd brcmfmac_5.4.y-nexmon
+mv $(modinfo brcmfmac -n) ./brcmfmac.ko.orig
+cp ./brcmfmac.ko $(modinfo brcmfmac -n)
+depmod -a
+info "Done."
+
+info "Downloading additional scripts"
+cd $_pwd
+wget https://raw.githubusercontent.com/zeroby0/nexmon_csi/pi-5.4.51-plus/update.sh -O update.sh
+info "Done."
+info "Completed"
